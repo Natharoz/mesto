42cc04cca67ba040777438e1e3ff200120382f10
Natharos <natharos93@yandex.ru>
2024-01-17 13:49:57 +0500

    fix: implement correct loading popup data after close button

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
 // Открытие/закрытие попапа редактирования профиля
 let editPopup = document.querySelector('.popup');
 let profileEditButton = document.querySelector('.profile__edit-button');
-
-profileEditButton.addEventListener('click', function(){
-  openPopup(editPopup);
-});
-
 let profileCloseButton = document.querySelector('.popup__close-button');
-
-profileCloseButton.addEventListener('click', function(){
-  closePopup(editPopup);
-});
-
-// Открытие/закрытие попапа
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-};
-
-function closePopup(popup) {
-  popup.classList.remove ('popup_opened');
-};
 
 // Информация об авторе
 let author = {
@@ -39,31 +21,33 @@ authorDescription.textContent = author.description;
 let authorDescriptionInput = document.querySelector('.popup__field[name="occupation"]');
 authorDescriptionInput.value = author.description;
 
-// Copypast
+// Открытие/закрытие попапа
+profileEditButton.addEventListener('click', function(){
+  openPopup(editPopup);
+  authorNameInput.value = authorName.textContent;
+  authorDescriptionInput.value = authorDescription.textContent;
+});
 
+profileCloseButton.addEventListener('click', function(){
+  closePopup(editPopup);
+});
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+// Сохранение данных попап
 function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-												// Так мы можем определить свою логику отправки.
-												// О том, как это делать, расскажем позже.
-
-	// Находим поля формы в DOM
-	// let nameInput = // Воспользуйтесь инструментом .querySelector()
-	// let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-	// Получите значение полей из свойства value
-  authorNameInput.getAttribute('value');
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
-  authorName.textContent =
+  evt.preventDefault();
+  let authorNameValue = authorNameInput.value;
+  authorName.textContent = authorNameValue;
+  let authorDescriptionValue = authorDescriptionInput.value;
+  authorDescription.textContent = authorDescriptionValue;
+  closePopup(editPopup);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+editPopup.addEventListener('submit', formSubmitHandler);
