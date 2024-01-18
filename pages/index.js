0663ef8f0a35a18b4c101597bbcbc97496a80610
Natharos <natharos93@yandex.ru>
2024-01-18 11:18:04 +0500

    feat: add like js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-// Открытие/закрытие попапа редактирования профиля
+// Попап
 let editPopup = document.querySelector('.popup');
 let profileEditButton = document.querySelector('.profile__edit-button');
 let profileCloseButton = document.querySelector('.popup__close-button');
@@ -21,20 +21,21 @@ authorDescription.textContent = author.description;
 let authorDescriptionInput = document.querySelector('.popup__field[name="occupation"]');
 authorDescriptionInput.value = author.description;
 
-// Открытие/закрытие попапа
+// Открытие попапа
 profileEditButton.addEventListener('click', function(){
   openPopup(editPopup);
   authorNameInput.value = authorName.textContent;
   authorDescriptionInput.value = authorDescription.textContent;
 });
 
-profileCloseButton.addEventListener('click', function(){
-  closePopup(editPopup);
-});
-
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 };
+
+// Закрытие попапа
+profileCloseButton.addEventListener('click', function(){
+  closePopup(editPopup);
+});
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
@@ -48,6 +49,16 @@ function formSubmitHandler (evt) {
   let authorDescriptionValue = authorDescriptionInput.value;
   authorDescription.textContent = authorDescriptionValue;
   closePopup(editPopup);
-}
+};
 
 editPopup.addEventListener('submit', formSubmitHandler);
+// Попап конец
+
+// Лайк
+let elements = document.querySelector('.elements');
+
+elements.onclick = function(event){
+  if (event.target.ariaLabel === 'нравится'){
+    event.target.classList.toggle('element__like_active')
+  }
+};
